7cf4874d59135bc1862c191525e7048f2e951444
David Tran <davidlamt@gmail.com>
2016-04-17 12:09:15 -0700

    Add session to users routes

--- routes/users.js
+++ routes/users.js
@@ -13,8 +13,7 @@ router.post('/', (req, res) => {
         const newUser = { first_name, last_name, email, username, password: hashedPassword };
 
         userUtils.createUser(newUser).then((createdUser) => {
-            req.session.username = createdUser.username;
-            req.session.status = createdUser.status;
+            req.session.user = createdUser;
 
             res.json(createdUser);
         }, () => res.sendStatus(400));
@@ -43,15 +42,22 @@ router.put('/:id', userAuth, (req, res) => {
         const newUserInfo = { first_name, last_name, email, username, password: hashedPassword };
 
         userUtils.updateUser(userID, newUserInfo).then((updatedUser) => {
+            const { _id, created_at, status } = req.session.user;
+            req.session.user = newUserInfo;
+            req.session.user._id = _id;
+            req.session.user.created_at = created_at;
+            req.session.user.status = status;
+            console.log(req.session);
             res.json(updatedUser);
-        }, (err) => res.sendStatus(400));
+        }, (err) => res.sendStatus(404));
     }, () => res.sendStatus(500));
 });
 
-router.delete('/:id', userAuth, (req, res) => {
+router.delete('/:id', adminAuth, (req, res) => {
     const userID = req.params.id;
 
     userUtils.deleteUser(userID).then((deletedUser) => {
+        req.session.destroy();
         res.json(deletedUser);
     }, () => res.sendStatus(404));
 });
