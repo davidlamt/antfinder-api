0b232663c4c4bcb5a4f99304fd50d480495aafe9
David Tran <davidlamt@gmail.com>
2016-04-30 14:38:43 -0700

    Add access control settings to allow cross-domain requests

--- app.js
+++ app.js
@@ -39,6 +39,13 @@ app.use(session({
         collection: 'sessions'
     })
 }));
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+    next();
+});
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/authenticate', authenticate);
