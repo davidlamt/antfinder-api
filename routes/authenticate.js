3e894585c5e5ca1f5afeb4cec33601f418ef6320
David Tran <davidlamt@gmail.com>
2016-04-30 14:41:07 -0700

    Remove unncessary console logs

--- routes/authenticate.js
+++ routes/authenticate.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-    console.log(req.session.user);
     if (req.session.user) return res.sendStatus(200);
 
     res.sendStatus(404);
