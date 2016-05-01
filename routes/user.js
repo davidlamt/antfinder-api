c6474d821a43a184da03b036c6eef7ef3b40560b
David Tran <davidlamt@gmail.com>
2016-04-30 17:14:44 -0700

    Updated authentications to deal with object arrays and objects

--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,14 @@ var router = express.Router();
 import { userAuth, adminAuth } from '../utilities/auth';
 
 router.get('/', userAuth, (req, res) => {
-    const { first_name: firstName, last_name: lastName, email, username, created_at: createdAt } = req.session.user[0];
+    let user;
+
+    if (req.session.user) {
+        if (!req.session.user.length) user = req.session.user;
+        else user = req.session.user[0];
+    }
+
+    const { first_name: firstName, last_name: lastName, email, username, created_at: createdAt } = user;
     const nonPrivateUserInfo = { firstName, lastName, email, username, createdAt };
 
     res.json(nonPrivateUserInfo);
