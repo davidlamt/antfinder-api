9491592b21ef950d12309a48ada6ce76ab2f09eb
David Tran <davidlamt@gmail.com>
2016-05-05 16:27:36 -0700

    Add cookie expiration time (1 hour)

--- app.js
+++ app.js
@@ -29,6 +29,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(session({
+    cookie: {
+        maxAge: 1000 * 60 * 60 // Expires after one hour
+    },
     secret: process.env.SESSION_SECRET,
     resave: true,
     saveUninitialized: true,
